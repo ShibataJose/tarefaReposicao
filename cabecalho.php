55c31b780d3737f7b89c7862ffc4ff1ec33b61db
ShibataJose <jose_shibata@hotmail.com>
2022-09-24 17:21:55 -0300

    Formatação do Arquivo cabecalho.php

--- cabecalho.php
+++ cabecalho.php
@@ -2,32 +2,38 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <title><?php echo $titulo?></title>
-    <link href = "./css/bootstrap.min.css" rel = "stylesheet"></link>
+    <title>
+        <?php echo $titulo?>
+    </title>
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+    </link>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="./site.php">
-    <img src="./pecas-de-reposicao.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-      <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="./index.php">Inicial</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="./disciplinas.php">Disciplinas</a>
-        </li>
-        <li class="nav-item ">
-          <a class="nav-link active" aria-current="page" href="./salas.php">Salas</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="./site.php">
+                <img src="./pecas-de-reposicao.png" alt="Logo" width="30" height="24"
+                    class="d-inline-block align-text-top">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="./index.php">Inicial</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="./disciplinas.php">Disciplinas</a>
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link active" aria-current="page" href="./salas.php">Salas</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
     
 <div class="ctn">
